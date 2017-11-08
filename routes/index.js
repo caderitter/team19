e3ce96000d24710832b29c0728dda140a12c6839
Brandon Walker <bcw58@cornell.edu>
2017-11-08 06:44:43 -0500

    Primitive Twitter data scraping logged to the console

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 var ToneAnalyzer = require('watson-developer-cloud/tone-analyzer/v3');
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
+var Twitter = require('twitter');
 var db = require('../db');
+
+const CONSUMER_KEY = 'PtJSr4p9QagFXYhr3C93xMgX3';
+const CONSUMER_SECRET = 'MN5F0GG3xudTcVun2Rm6W1UHCwIYcXvXr4qQAI5rm35BnkeSl1';
+const ACCESS_TOKEN_KEY = '921107203871866880-dAZabMibKNaeYmXTA7L0YPL6iCmLjmo';
+const ACCESS_TOKEN_SECRET = '2xJPmz0I0fbGA4HG4XLbfxWV3HxeaSQGZZi0wmuDXbjpy';
 
 const tone_analyzer = new ToneAnalyzer({
     username: 'd609b749-ea64-4f98-8d02-f8066222a35a',
@@ -61,6 +67,41 @@ router.post('/nlu', function (req, res) {
 });
 
 
+/* Setup Twitter */
+var client = new Twitter({
+    consumer_key: CONSUMER_KEY,
+    consumer_secret: CONSUMER_SECRET,
+    access_token_key: ACCESS_TOKEN_KEY,
+    access_token_secret: ACCESS_TOKEN_SECRET
+});
+
+/* Twitter Request */
+var name = '%23IBMfinishline17';
+// client.get('statuses/user_timeline', {screen_name: "IBM"}, function(error, tweets, response) {
+//     if (error) {
+//         console.log("Error: " + error);
+//     } else {
+//         console.log("TWEETS");
+//         console.log(tweets);
+//     }
+// });
+
+client.get('search/tweets/', {q: name, count: 100}, function(error, tweets, response) {
+    if (error) {
+        console.log("Error: " + error);
+        console.log(tweets);
+    } else {
+        console.log("Number of results: " + tweets.search_metadata.count);
+        console.log(tweets.statuses.length);
+
+        for (i in tweets.statuses) {
+            var tweet = tweets.statuses[i];
+            console.log("truncated: " + tweet.truncated);
+            console.log(tweet.text);
+        }
+        // console.log(tweets);
+    }
+});
 
 
 /* GET home page. */
